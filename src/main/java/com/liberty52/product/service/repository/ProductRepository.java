06a6b3f7e4ec7f9820d25f751e94547fcb48de43
ansm6358 <ansm6358@naver.com>
2023-04-14 16:50:08 +0900

    수정 및 테스트 완료

--- src/main/java/com/liberty52/product/service/repository/ProductRepository.java
+++ src/main/java/com/liberty52/product/service/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.liberty52.product.service.repository;
 import com.liberty52.product.service.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByName(String productName);
 }
