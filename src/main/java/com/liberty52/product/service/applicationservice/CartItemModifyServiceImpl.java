dbb3e2098d4ad3119aea8836cd44b94c1a3e5cbd
parkcoldroad <coldstreet@naver.com>
2023-04-16 20:24:27 +0900

    refactor: 장바구니 수정 관련 메소드 함수화

--- src/main/java/com/liberty52/product/service/applicationservice/CartItemModifyServiceImpl.java
+++ src/main/java/com/liberty52/product/service/applicationservice/CartItemModifyServiceImpl.java
@@ -1,10 +1,8 @@
 package com.liberty52.product.service.applicationservice;
 
-import static org.springframework.util.ObjectUtils.isEmpty;
-
 import com.liberty52.product.global.adapter.S3Uploader;
 import com.liberty52.product.global.exception.external.CustomProductNotFoundExcpetion;
-import com.liberty52.product.global.exception.external.NotYourResourceException;
+import com.liberty52.product.global.exception.external.NotYourResource;
 import com.liberty52.product.global.exception.external.OptionDetailNotFoundException;
 import com.liberty52.product.global.exception.external.OrderItemCannotModifiedException;
 import com.liberty52.product.service.controller.dto.CartModifyRequestDto;
@@ -14,60 +12,45 @@ import com.liberty52.product.service.entity.OptionDetail;
 import com.liberty52.product.service.repository.CustomProductOptionRepository;
 import com.liberty52.product.service.repository.CustomProductRepository;
 import com.liberty52.product.service.repository.OptionDetailRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Service
-@Transactional
-public class CartItemModifyServiceImpl implements CartItemModifyService {
+public class CartItemModifyServiceImpl implements CartItemModifyService{
+
   private final S3Uploader s3Uploader;
   private final CustomProductRepository customProductRepository;
   private final OptionDetailRepository optionDetailRepository;
 
   private final CustomProductOptionRepository customProductOptionRepository;
 
+  @Transactional
   @Override
-  public void modifyCartItemList(String authId, List<CartModifyRequestDto> dto, List<MultipartFile> imageFile) {
-    modifyCartItem(authId, dto, imageFile);
-  }
-
-  @Override
-  public void modifyGuestCartItemList(String guestId, List<CartModifyRequestDto> dto, List<MultipartFile> imageFile) {
-    modifyCartItem(guestId, dto, imageFile);
-  }
-
-  private void modifyCartItem(String ownerId, List<CartModifyRequestDto> cmrdDto, List<MultipartFile> imageFile) {
-    for (int i=0;i< cmrdDto.size();i++){
-      CustomProduct customProduct = customProductRepository.findById(cmrdDto.get(i).getCustomProductId())
-          .orElseThrow((CustomProductNotFoundExcpetion::new));
-      validCartItem(ownerId, customProduct);
-      modifyOptionsDetail(cmrdDto.get(i), customProduct, imageFile.get(i));
-    }
-  }
-  private static void validCartItem(String ownerId, CustomProduct customProduct) {
-    if (customProduct.isInOrder()) {
+  public void modifyCartItem(String authId, CartModifyRequestDto dto, MultipartFile imageFile, String customProductId) {
+    CustomProduct customProduct = customProductRepository.findById(customProductId).orElseThrow((CustomProductNotFoundExcpetion::new));
+    if(customProduct.isInOrder()){
       throw new OrderItemCannotModifiedException();
     }
 
-    if (!customProduct.getCart().getAuthId().equals(ownerId)) {
-      throw new NotYourResourceException("customProduct", ownerId);
+    if(!customProduct.getCart().getAuthId().equals(authId)){
+      throw new NotYourResource("customProduct",authId);
     }
-  }
 
-  private void modifyOptionsDetail(CartModifyRequestDto dto, CustomProduct customProduct, MultipartFile imageFile) {
-    if (!isEmpty(imageFile) && imageFile.getSize() != 0) {
-      String customPictureUrl = uploadImage(imageFile);
+    String customPictureUrl = uploadImage(imageFile);
+    if (customPictureUrl != null){
       customProduct.modifyCustomPictureUrl(customPictureUrl);
     }
-    customProduct.modifyQuantity(dto.getQuantity());
 
+    customProduct.modifyQuantity(dto.getQuantity());
+    modifyOptionsDetail(dto, customProduct);
+  }
+
+  private void modifyOptionsDetail(CartModifyRequestDto dto, CustomProduct customProduct) {
     customProductOptionRepository.deleteAll(customProduct.getOptions());
-    for (String optionDetailName : dto.getOptions()) {
+    for (String optionDetailName : dto.getOptions()){
       CustomProductOption customProductOption = CustomProductOption.create();
       OptionDetail optionDetail = optionDetailRepository.findByName(optionDetailName)
           .orElseThrow(() -> new OptionDetailNotFoundException(optionDetailName));
@@ -78,7 +61,7 @@ public class CartItemModifyServiceImpl implements CartItemModifyService {
   }
 
   private String uploadImage(MultipartFile multipartFile) {
-    if (multipartFile == null) {
+    if(multipartFile == null) {
       return null;
     }
     return s3Uploader.upload(multipartFile);
