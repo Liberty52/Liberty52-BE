588a92f66d0f402173c8646e241c3bfd175f844f
kyoohyun <kgh2120@naver.com>
2023-04-17 09:26:36 +0900

    주문 조회 대표 이미지 고정 값으로 추가

---
+++ src/main/java/com/liberty52/product/global/contants/RepresentImageUrl.java
@@ -0,0 +1,5 @@
+package com.liberty52.product.global.contants;
+
+public class RepresentImageUrl {
+    public static final String LIBERTY52_FRAME_REPRESENTATIVE_URL = "https://liberty52.s3.ap-northeast-2.amazonaws.com/product/represent/liberty52-frame.png";
+}
