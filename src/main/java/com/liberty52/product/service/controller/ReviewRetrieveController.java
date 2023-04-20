b2ce3a9ccb5d53249e0e767612b54e2fddb35ea3
kyoohyun <kgh2120@naver.com>
2023-04-20 18:37:21 +0900

    리뷰 조회 Repository 리팩토링

--- src/main/java/com/liberty52/product/service/controller/ReviewRetrieveController.java
+++ src/main/java/com/liberty52/product/service/controller/ReviewRetrieveController.java
@@ -1,14 +1,12 @@
 package com.liberty52.product.service.controller;
 
 
-
 import com.liberty52.product.service.applicationservice.ReviewRetrieveService;
 import com.liberty52.product.service.controller.dto.ReviewRetrieveResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/liberty52/product/service/repository/ReviewQueryRepositoryImpl.java
+++ src/main/java/com/liberty52/product/service/repository/ReviewQueryRepositoryImpl.java
@@ -1,20 +1,17 @@
 package com.liberty52.product.service.repository;
 
 import static com.liberty52.product.service.entity.QOrders.orders;
-import static com.liberty52.product.service.entity.QProduct.product;
 import static com.liberty52.product.service.entity.QReply.reply;
 import static com.liberty52.product.service.entity.QReview.review;
 import static com.liberty52.product.service.entity.QReviewImage.reviewImage;
 
 import com.liberty52.product.service.controller.dto.ReviewRetrieveResponse;
-import com.liberty52.product.service.entity.QOrders;
-import com.liberty52.product.service.entity.QProduct;
-import com.liberty52.product.service.entity.QReview;
 import com.liberty52.product.service.entity.Review;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import java.util.HashMap;
 import java.util.List;
-import org.springframework.data.domain.PageImpl;
+import java.util.Map;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +23,10 @@ public class ReviewQueryRepositoryImpl implements ReviewQueryRepository{
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
 
+    private final String currentPage = "currentPage";
+    private final String startPage = "startPage";
+    private final String lastPage = "lastPage";
+
     public ReviewQueryRepositoryImpl(EntityManager em){
         this.em = em;
         this.queryFactory = new JPAQueryFactory(this.em);
@@ -33,9 +34,24 @@ public class ReviewQueryRepositoryImpl implements ReviewQueryRepository{
 
     public ReviewRetrieveResponse retrieveReview(String productId, String authorId, Pageable pageable){
 
+        List<Review> reviews = fetchReviews(productId, pageable);
+
+        if(reviews.isEmpty())
+            return noReviewExistCase(authorId, reviews); // 리뷰가 0인 경우에 리턴하는 케이스
+
+        Map<String, Long> pageInfo = getPageInfo(pageable, productId);
+
+        return new ReviewRetrieveResponse(reviews,
+                pageInfo.get(startPage),
+                pageInfo.get(currentPage),
+                pageInfo.get(lastPage)
+                ,authorId);
 
 
-        List<Review> reviews = queryFactory.selectFrom(review)
+    }
+
+    private List<Review> fetchReviews(String productId, Pageable pageable) {
+        return queryFactory.selectFrom(review)
                 .leftJoin(reply).on(reply.review.eq(review)).fetchJoin()
                 .leftJoin(reviewImage).on(reviewImage.review.eq(review)).fetchJoin()
                 .leftJoin(orders).on(review.order.eq(orders)).fetchJoin()
@@ -43,25 +59,32 @@ public class ReviewQueryRepositoryImpl implements ReviewQueryRepository{
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
-
-
-        int currentPage = pageable.getPageNumber() +1;
-        int startPage = 10 * (currentPage/10) +1 ; // 1 11 21 31..
-
-        Long total = queryFactory.select(review.count())
+    }
+    private Long getTotalCount(String productId) {
+        return queryFactory.select(review.count())
                 .from(review)
                 .where(review.product.id.eq(productId))
                 .fetchOne();
+    }
 
+    private Map<String,Long> getPageInfo(Pageable pageable, String productId){
+        long currentPage = pageable.getPageNumber() +1;
+        long startPage = 10 * (currentPage/10) +1 ; // 1 11 21 31..
+        Long total = getTotalCount(productId);
         long totalLastPage = total%pageable.getPageSize() == 0 ? total / pageable.getPageSize() :
                 total / pageable.getPageSize()+1;
-
-
         long lastPage = Math.min(totalLastPage,
                 10L * (currentPage%10 == 0 ? currentPage/10 : currentPage / 10 + 1));
-        return new ReviewRetrieveResponse(reviews, currentPage, startPage, lastPage,authorId);
+        Map<String,Long> returnMap = new HashMap<>();
+        returnMap.put(this.startPage,startPage);
+        returnMap.put(this.currentPage,currentPage);
+        returnMap.put(this.lastPage,lastPage);
+        return returnMap;
+    }
 
-
+    private ReviewRetrieveResponse noReviewExistCase(String authorId,
+            List<Review> reviews) {
+        return new ReviewRetrieveResponse(reviews, 0, 0, 0, authorId);
     }
 
 }
