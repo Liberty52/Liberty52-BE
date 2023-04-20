6497d0a0ea52dfe5cb5e2f886dded84b6f729a81
kyoohyun <kgh2120@naver.com>
2023-04-20 19:26:41 +0900

    FeignClient용 API 생성

--- src/main/java/com/liberty52/product/global/adapter/AuthClient.java
+++ src/main/java/com/liberty52/product/global/adapter/AuthClient.java
@@ -1,7 +1,6 @@
 package com.liberty52.product.global.adapter;
 
 import com.liberty52.product.service.controller.dto.AuthClientDataResponse;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient("auth")
 public interface AuthClient {
 
-//    @PostMapping("/auth/")
+    @PostMapping("/info")
     Map<String, AuthClientDataResponse> retrieveAuthData(@RequestBody Set<String> ids);
 
 }
