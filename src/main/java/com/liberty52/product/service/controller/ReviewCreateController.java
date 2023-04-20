a2bcc916bfd40d56ed3761fb122dc0fce9739e90
parkcoldroad <coldstreet@naver.com>
2023-04-20 17:16:01 +0900

    feat: 리뷰 추가 컨트롤러 생성

---
+++ src/main/java/com/liberty52/product/service/controller/ReviewCreateController.java
@@ -0,0 +1,30 @@
+package com.liberty52.product.service.controller;
+
+import com.liberty52.product.service.applicationservice.ReviewCreateService;
+import com.liberty52.product.service.controller.dto.ReviewCreateRequestDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewCreateController {
+
+  private final ReviewCreateService reviewCreateService;
+
+  @PostMapping("/reviews")
+  @ResponseStatus(HttpStatus.CREATED)
+  public void reviewCreate(@RequestHeader(HttpHeaders.AUTHORIZATION) String reviewerId, @Validated @RequestPart ReviewCreateRequestDto dto,
+      @RequestPart(value = "file",required = false) List<MultipartFile> imageFile) {
+    reviewCreateService.createReview(reviewerId,dto,imageFile);
+  }
+}
