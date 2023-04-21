d0e949290db1ca2465942415a8581d5dd3f34445
ansm6358 <ansm6358@naver.com>
2023-04-21 18:45:26 +0900

    리뷰 삭제 테스트

--- src/test/java/com/liberty52/product/service/applicationservice/ReviewRemoveServiceTest.java
+++ src/test/java/com/liberty52/product/service/applicationservice/ReviewRemoveServiceTest.java
@@ -1,10 +1,22 @@
 package com.liberty52.product.service.applicationservice;
 
+import com.liberty52.product.global.config.DBInitConfig;
+import com.liberty52.product.global.exception.external.NotMatchReviewerIdException;
+import com.liberty52.product.global.exception.external.OptionDetailNotFoundException;
+import com.liberty52.product.global.exception.external.ReviewNotFoundException;
+import com.liberty52.product.service.entity.Orders;
+import com.liberty52.product.service.entity.Product;
+import com.liberty52.product.service.entity.Review;
+import com.liberty52.product.service.entity.ReviewImage;
 import com.liberty52.product.service.repository.ReviewRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @Transactional
@@ -16,8 +28,33 @@ public class ReviewRemoveServiceTest {
     @Autowired
     ReviewRemoveService reviewRemoveService;
 
+    private Product product;
+    private Orders order;
+    private Review review;
+
+    @BeforeEach
+    void beforeEach() {
+        product = DBInitConfig.DBInitService.getProduct();
+        order = DBInitConfig.DBInitService.getOrder();
+
+        review = Review.create(4, "content");
+        review.associate(order);
+        review.associate(product);
+        reviewRepository.save(review);
+
+    }
+
     @Test
     void 리뷰삭제() {
+        Review reviewBefore = reviewRepository.findById(review.getId()).orElse(null);
+        Assertions.assertNotNull(reviewBefore);
+
+        Assertions.assertThrows(ReviewNotFoundException.class, () -> reviewRemoveService.removeReview("authId", "123"));
+        Assertions.assertThrows(NotMatchReviewerIdException.class, () -> reviewRemoveService.removeReview("123", review.getId()));
+
+        reviewRemoveService.removeReview("authId", review.getId());
+        Review reviewAfter = reviewRepository.findById(review.getId()).orElse(null);
+        Assertions.assertNull(reviewAfter);
 
     }
 }
