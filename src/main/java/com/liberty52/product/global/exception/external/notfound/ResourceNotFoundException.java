b928daae6da869afbc06dac6fe535d972b456c98
jinlee0 <wls4370@gmail.com>
2023-04-25 21:50:14 +0900

    refactor: NouFoundException 구현

--- src/main/java/com/liberty52/product/global/exception/external/notfound/ResourceNotFoundException.java
+++ src/main/java/com/liberty52/product/global/exception/external/notfound/ResourceNotFoundException.java
@@ -1,10 +1,7 @@
 package com.liberty52.product.global.exception.external.notfound;
 
-import com.liberty52.product.global.exception.external.AbstractApiException;
-import com.liberty52.product.global.exception.external.ProductErrorCode;
-
-public class ResourceNotFoundException extends AbstractApiException {
+public class ResourceNotFoundException extends NotFoundException {
     public ResourceNotFoundException(String resourceName, String by, String param) {
-        super(ProductErrorCode.RESOURCE_NOT_FOUND, String.format("%s is not found by %s. The %s was %s", resourceName, by, by, param));
+        super(String.format("%s is not found by %s. The %s was %s", resourceName, by, by, param));
     }
 }
