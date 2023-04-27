1dc4d4ba968a6ba31573e8126e303d14d707fa9f
Seungho <hsh47607@naver.com>
2023-04-27 14:19:43 +0900

    modify stub auth service response email

--- src/test/java/com/liberty52/product/stub/StubAuthServiceClient.java
+++ src/test/java/com/liberty52/product/stub/StubAuthServiceClient.java
@@ -6,6 +6,6 @@ import com.liberty52.product.global.adapter.cloud.dto.AuthProfileDto;
 public class StubAuthServiceClient implements AuthServiceClient {
     @Override
     public AuthProfileDto getAuthProfile(String authId) {
-        return new AuthProfileDto("hsh47607@naver.com", "황승호");
+        return new AuthProfileDto("mju.omnm@gmail.com", "빌드 테스터");
     }
 }
