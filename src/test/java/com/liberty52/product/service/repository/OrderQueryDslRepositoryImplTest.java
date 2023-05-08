60cb657d97bc18c91bd4acc7b115d370ff0f382e
kyoohyun <kgh2120@naver.com>
2023-05-08 22:17:13 +0900

    FIX - #188  Filter OrderStatus-Ready when retrieve order

---
+++ src/test/java/com/liberty52/product/service/repository/OrderQueryDslRepositoryImplTest.java
@@ -0,0 +1,56 @@
+package com.liberty52.product.service.repository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.liberty52.product.service.entity.OrderDestination;
+import com.liberty52.product.service.entity.Orders;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class OrderQueryDslRepositoryImplTest {
+
+
+    @Autowired
+    EntityManager em;
+
+
+    OrderQueryDslRepository repository;
+    @BeforeEach
+    void beforeEach(){
+        repository = new OrderQueryDslRepositoryImpl(em);
+    }
+
+    @Test
+    void ready_filtering_test () throws Exception{
+        //given
+        final String AUTH_ID = "user";
+
+        for (int i = 0; i < 10; i++) {
+
+            OrderDestination destination = OrderDestination.create("receiver", "", "", "", "", "");
+            em.persist(Orders.create(AUTH_ID,destination));
+
+        }
+        OrderDestination destination = OrderDestination.create("receiver", "", "", "", "", "");
+        Orders orders = Orders.create(AUTH_ID, destination);
+        orders.changeOrderStatusToOrdered();
+        em.persist(orders);
+        em.flush();
+
+        //when
+        List<Orders> list = repository.retrieveOrders(AUTH_ID);
+
+        //then
+        assertThat(list).hasSize(1);
+
+
+    }
+
+}
