abcd68c5c12cc23c68ddc84111f7a42308556461
hseungho <hsh47607@naver.com>
2023-05-19 23:46:22 +0900

    Feat: Order Refund by Admin API

--- src/main/java/com/liberty52/product/global/exception/external/badrequest/OrderCancelException.java
+++ src/main/java/com/liberty52/product/global/exception/external/badrequest/OrderCancelException.java
@@ -2,8 +2,8 @@ package com.liberty52.product.global.exception.external.badrequest;
 
 import com.liberty52.product.service.entity.OrderStatus;
 
-public class CannotOrderCancelException extends BadRequestException {
-    public CannotOrderCancelException(OrderStatus nowStatus) {
+public class OrderCancelException extends BadRequestException {
+    public OrderCancelException(OrderStatus nowStatus) {
         super("해당 주문은 취소할 수 없는 상태입니다. 현재상태: " + nowStatus.name());
     }
 }
