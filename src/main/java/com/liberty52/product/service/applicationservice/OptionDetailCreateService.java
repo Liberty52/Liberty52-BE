41efb1e0c478275c41462f61f3aa5cdf4e586d97
ansm6358 <ansm6358@naver.com>
2023-05-20 20:38:48 +0900

    옵션 선택지 추가

--- src/main/java/com/liberty52/product/service/applicationservice/OptionDetailCreateService.java
+++ src/main/java/com/liberty52/product/service/applicationservice/OptionDetailCreateService.java
@@ -1,2 +1,7 @@
-package com.liberty52.product.service.applicationservice;public interface OptionDetailCreateService {
+package com.liberty52.product.service.applicationservice;
+
+import com.liberty52.product.service.controller.dto.CreateOptionDetailRequestDto;
+
+public interface OptionDetailCreateService {
+    void createOptionDetail(String role, CreateOptionDetailRequestDto dto, String optionId);
 }
