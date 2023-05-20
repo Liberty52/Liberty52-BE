a3025496b1a0906be933498c0ccc9d82c8b7f2dc
ansm6358 <ansm6358@naver.com>
2023-05-20 23:16:09 +0900

    옵션 선택지 삭제 서비스

--- src/main/java/com/liberty52/product/service/controller/dto/OptionDetailRemoveRequestDto.java
+++ src/main/java/com/liberty52/product/service/controller/dto/OptionDetailRemoveRequestDto.java
@@ -1,2 +1,17 @@
-package com.liberty52.product.service.controller.dto;public class OptionDetailRemoveRequestDto {
+package com.liberty52.product.service.controller.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class OptionDetailRemoveRequestDto {
+
+    @NotNull
+    Boolean onSail;
+
+    public static OptionDetailRemoveRequestDto create(boolean onSail){
+        OptionDetailRemoveRequestDto dto = new OptionDetailRemoveRequestDto();
+        dto.onSail = onSail;
+        return dto;
+    }
 }
