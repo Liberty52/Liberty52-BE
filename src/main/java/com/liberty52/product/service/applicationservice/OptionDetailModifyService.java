db91c066b48e4e0d8f9ff90dcdbb1b2b2ea7f5de
ansm6358 <ansm6358@naver.com>
2023-05-26 17:58:51 +0900

    옵션 선택지 수정

---
+++ src/main/java/com/liberty52/product/service/applicationservice/OptionDetailModifyService.java
@@ -0,0 +1,7 @@
+package com.liberty52.product.service.applicationservice;
+
+import com.liberty52.product.service.controller.dto.OptionDetailModifyRequestDto;
+
+public interface OptionDetailModifyService {
+    void modifyOptionDetailByAdmin(String role, String optionDetailId, OptionDetailModifyRequestDto dto);
+}
---
+++ src/main/java/com/liberty52/product/service/controller/OptionDetailModifyController.java
@@ -0,0 +1,21 @@
+package com.liberty52.product.service.controller;
+
+import com.liberty52.product.service.applicationservice.OptionDetailModifyService;
+import com.liberty52.product.service.controller.dto.OptionDetailModifyRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class OptionDetailModifyController {
+
+    private final OptionDetailModifyService optionDetailModifyService;
+
+    @PatchMapping("/admin/optionDetail/{optionDetailId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void modifyOptionDetailByAdmin(@RequestHeader("LB-Role") String role, @PathVariable String optionDetailId, @Validated @RequestBody OptionDetailModifyRequestDto dto) {
+        optionDetailModifyService.modifyOptionDetailByAdmin(role, optionDetailId, dto);
+    }
+}
