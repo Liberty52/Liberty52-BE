8ae5aade8685e023c566da07e5db4853e272a481
hseungho <hsh47607@naver.com>
2023-05-28 02:33:15 +0900

    add: vbank delete service

---
+++ src/main/java/com/liberty52/product/service/applicationservice/VBankDeleteService.java
@@ -0,0 +1,5 @@
+package com.liberty52.product.service.applicationservice;
+
+public interface VBankDeleteService {
+    void deleteVBank(String role, String vBankId);
+}
---
+++ src/main/java/com/liberty52/product/service/applicationservice/impl/VBankDeleteServiceImpl.java
@@ -0,0 +1,35 @@
+package com.liberty52.product.service.applicationservice.impl;
+
+import com.liberty52.product.global.exception.external.badrequest.BadRequestException;
+import com.liberty52.product.global.exception.external.notfound.ResourceNotFoundException;
+import com.liberty52.product.global.util.Validator;
+import com.liberty52.product.service.applicationservice.VBankDeleteService;
+import com.liberty52.product.service.entity.payment.VBank;
+import com.liberty52.product.service.repository.VBankRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VBankDeleteServiceImpl implements VBankDeleteService {
+
+    private final VBankRepository vBankRepository;
+
+    @Override
+    public void deleteVBank(String role, String vBankId) {
+        Validator.isAdmin(role);
+
+        VBank vBank = vBankRepository.findById(vBankId)
+                .orElseThrow(() -> new ResourceNotFoundException("VBANK", "id", vBankId));
+
+        validateDeleteVBank();
+
+        vBankRepository.delete(vBank);
+    }
+
+    private void validateDeleteVBank() {
+        if (vBankRepository.countAll() <= 1L) {
+            throw new BadRequestException("가상계좌는 반드시 1개 이상 존재해야 합니다.");
+        }
+    }
+}
