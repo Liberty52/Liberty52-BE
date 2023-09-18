ffb958887c258a96b905e60092cbf836502d3603
ansm6358 <ansm6358@naver.com>
2023-09-19 03:20:51 +0900

    카트 추가 테스트 mock테스트 추가

--- src/test/java/com/liberty52/product/service/applicationservice/mock/CartItemCreateServiceMockTest.java
+++ src/test/java/com/liberty52/product/service/applicationservice/mock/CartItemCreateServiceMockTest.java
@@ -1,16 +1,21 @@
-package com.liberty52.product.service.applicationservice;
+package com.liberty52.product.service.applicationservice.mock;
 
 import com.liberty52.product.MockS3Test;
+import com.liberty52.product.global.adapter.s3.S3UploaderApi;
 import com.liberty52.product.global.exception.external.notfound.OptionDetailNotFoundByNameException;
 import com.liberty52.product.global.exception.external.notfound.ProductNotFoundByNameException;
+import com.liberty52.product.service.applicationservice.CartItemCreateService;
+import com.liberty52.product.service.applicationservice.impl.CartItemCreateServiceImpl;
 import com.liberty52.product.service.controller.dto.CartItemRequest;
-import com.liberty52.product.service.entity.Cart;
-import com.liberty52.product.service.entity.CustomProduct;
-import com.liberty52.product.service.entity.CustomProductOption;
-import com.liberty52.product.service.repository.CartItemRepository;
-import com.liberty52.product.service.repository.CartRepository;
+import com.liberty52.product.service.entity.*;
+import com.liberty52.product.service.repository.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
@@ -20,102 +25,110 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
-@SpringBootTest
-@Transactional
-public class CartItemCreateServiceTest extends MockS3Test {
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atMostOnce;
+import static org.mockito.Mockito.times;
 
-    @Autowired
-    CartItemCreateService cartItemCreateService;
+@ExtendWith(MockitoExtension.class)
+public class CartItemCreateServiceMockTest extends MockS3Test {
 
-    @Autowired
+    @InjectMocks
+    CartItemCreateServiceImpl cartItemCreateService;
+
+    @Mock
     CartRepository cartRepository;
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Mock
+    OptionDetailRepository optionDetailRepository;
+
+    @Mock
+    CustomProductRepository customProductRepository;
+
+    @Mock
+    CustomProductOptionRepository customProductOptionRepository;
 
     @Test
     void 장바구니생성() throws IOException {
 
-        CartItemRequest dto1 = new CartItemRequest();
+        //given
         String[] option = {"이젤 거치형", "1mm 두께 승화전사 인쇄용 알루미늄시트", "무광실버"};
-        dto1.create("Liberty 52_Frame", 1, option);
-
-        CartItemRequest dto2 = new CartItemRequest();
-        dto2.create("L", 2, option);
-
+        CartItemRequest dto1 = new CartItemRequest().builder().productName("Liberty 52_Frame").quantity(1).options(option).build();
+        CartItemRequest dto2 = new CartItemRequest().builder().productName("L").quantity(2).options(option).build();
         String[] optionErr = {"벽걸이형", "1mm 두께 승화전사 인쇄용 알루미늄시트", "무광레드"};
-        CartItemRequest dto3 = new CartItemRequest();
-        dto3.create("Liberty 52_Frame", 4, optionErr);
+        CartItemRequest dto3 = new CartItemRequest().builder().productName("Liberty 52_Frame").quantity(4).options(optionErr).build();
 
         MockMultipartFile imageFile = new MockMultipartFile("image", "test.png", "image/jpeg", new FileInputStream("src/test/resources/static/test.jpg"));
 
-        cartItemCreateService.createAuthCartItem("aaa", imageFile, dto1);
+        given(cartRepository.save(any())).willReturn(Cart.create("authId"));
 
-        Cart cart = cartRepository.findByAuthId("aaa").orElseThrow(() ->new RuntimeException());
-        Assertions.assertEquals(cart.getAuthId(), "aaa");
+        given(productRepository.findByName("Liberty 52_Frame")).willReturn(Optional.ofNullable(Product.create("Liberty 52_Frame",ProductState.ON_SALE,100L)));
 
-        CustomProduct customProduct = cart.getCustomProducts().get(0);
-        Assertions.assertEquals(customProduct.getQuantity(), 1);
-        Assertions.assertEquals(customProduct.getProduct().getName(), "Liberty 52_Frame");
+        given(customProductRepository.save(any())).willReturn(null);
+        given(customProductOptionRepository.save(any())).willReturn(null);
 
+        given(optionDetailRepository.findByName("이젤 거치형")).willReturn(Optional.ofNullable(OptionDetail.create("이젤 거치형", 100, true)));
+        given(optionDetailRepository.findByName("1mm 두께 승화전사 인쇄용 알루미늄시트")).willReturn(Optional.ofNullable(OptionDetail.create("1mm 두께 승화전사 인쇄용 알루미늄시트", 100, true)));
+        given(optionDetailRepository.findByName("무광실버")).willReturn(Optional.ofNullable(OptionDetail.create("무광실버", 100, true)));
+        given(optionDetailRepository.findByName("벽걸이형")).willReturn(Optional.ofNullable(OptionDetail.create("벽걸이형", 100, true)));
 
-        List<CustomProductOption> productCartOptionList = customProduct.getOptions();
-        System.out.println(customProduct.getOptions().size());
+        //when
+        cartItemCreateService.createAuthCartItem("authId", imageFile, dto1);
 
-        Assertions.assertEquals(productCartOptionList.get(0).getOptionDetail().getName(), "이젤 거치형");
-        Assertions.assertEquals(productCartOptionList.get(1).getOptionDetail().getName(), "1mm 두께 승화전사 인쇄용 알루미늄시트");
-        Assertions.assertEquals(productCartOptionList.get(2).getOptionDetail().getName(), "무광실버");
+        //then
+        Mockito.verify(cartRepository, atMostOnce()).findByAuthId(any());
+        Mockito.verify(optionDetailRepository, times(3)).findByName(any());
 
-        Assertions.assertThrows(ProductNotFoundByNameException.class, () -> cartItemCreateService.createAuthCartItem("aaa", imageFile, dto2));
+        //예외
+        Assertions.assertThrows(ProductNotFoundByNameException.class, () -> cartItemCreateService.createAuthCartItem("authId", imageFile, dto2));
 
+        Assertions.assertThrows(OptionDetailNotFoundByNameException.class, () -> cartItemCreateService.createAuthCartItem("authId", imageFile, dto3));
 
-        Assertions.assertThrows(OptionDetailNotFoundByNameException.class, () -> cartItemCreateService.createAuthCartItem("aaa", imageFile, dto3));
 
 
     }
 
     @Test
     void 게스트장바구니생성() throws IOException {
-
-        CartItemRequest dto1 = new CartItemRequest();
-        String[] option1 = {"이젤 거치형", "1mm 두께 승화전사 인쇄용 알루미늄시트", "무광실버"};
-        dto1.create("Liberty 52_Frame", 1, option1);
-
-        CartItemRequest dto2 = new CartItemRequest();
-        String[] option2 = {"벽걸이형", "1mm 두께 승화전사 인쇄용 알루미늄시트", "유광백색"};
-        dto2.create("Liberty 52_Frame", 2, option2);
+        //given
+        String[] option = {"이젤 거치형", "1mm 두께 승화전사 인쇄용 알루미늄시트", "무광실버"};
+        CartItemRequest dto1 = new CartItemRequest().builder().productName("Liberty 52_Frame").quantity(1).options(option).build();
+        CartItemRequest dto2 = new CartItemRequest().builder().productName("L").quantity(2).options(option).build();
+        String[] optionErr = {"벽걸이형", "1mm 두께 승화전사 인쇄용 알루미늄시트", "무광레드"};
+        CartItemRequest dto3 = new CartItemRequest().builder().productName("Liberty 52_Frame").quantity(4).options(optionErr).build();
 
         MockMultipartFile imageFile = new MockMultipartFile("image", "test.png", "image/jpeg", new FileInputStream("src/test/resources/static/test.jpg"));
 
+        given(cartRepository.save(any())).willReturn(Cart.create("testId"));
+
+        given(productRepository.findByName("Liberty 52_Frame")).willReturn(Optional.ofNullable(Product.create("Liberty 52_Frame",ProductState.ON_SALE,100L)));
+
+        given(customProductRepository.save(any())).willReturn(null);
+        given(customProductOptionRepository.save(any())).willReturn(null);
+
+        given(optionDetailRepository.findByName("이젤 거치형")).willReturn(Optional.ofNullable(OptionDetail.create("이젤 거치형", 100, true)));
+        given(optionDetailRepository.findByName("1mm 두께 승화전사 인쇄용 알루미늄시트")).willReturn(Optional.ofNullable(OptionDetail.create("1mm 두께 승화전사 인쇄용 알루미늄시트", 100, true)));
+        given(optionDetailRepository.findByName("무광실버")).willReturn(Optional.ofNullable(OptionDetail.create("무광실버", 100, true)));
+        given(optionDetailRepository.findByName("벽걸이형")).willReturn(Optional.ofNullable(OptionDetail.create("벽걸이형", 100, true)));
+
+        //when
         cartItemCreateService.createGuestCartItem("guest", imageFile, dto1);
-        cartItemCreateService.createGuestCartItem("guest", imageFile, dto2);
-
-        Cart cart = cartRepository.findByAuthId("guest").orElseThrow(() ->new RuntimeException());
-        Assertions.assertEquals(cart.getAuthId(), "guest");
-        Assertions.assertEquals(cart.getExpiryDate(), LocalDate.now().plusDays(7));
-        //Assertions.assertEquals(cart.getExpiryDate(), LocalDate.of(2023,04,27));
-
-        CustomProduct customProduct = cart.getCustomProducts().get(0);
-        Assertions.assertEquals(customProduct.getQuantity(), 1);
-        Assertions.assertEquals(customProduct.getProduct().getName(), "Liberty 52_Frame");
 
 
-        List<CustomProductOption> productCartOptionList = customProduct.getOptions();
+        //then
+        Mockito.verify(cartRepository, atMostOnce()).findByAuthIdAndExpiryDateGreaterThanEqual(any(), any());
+        //Mockito.verify(optionDetailRepository, times(3)).findByName(any());
 
-        Assertions.assertEquals(customProduct.getOptions().size(),3);
-        Assertions.assertEquals(productCartOptionList.get(0).getOptionDetail().getName(), "이젤 거치형");
-        Assertions.assertEquals(productCartOptionList.get(1).getOptionDetail().getName(), "1mm 두께 승화전사 인쇄용 알루미늄시트");
-        Assertions.assertEquals(productCartOptionList.get(2).getOptionDetail().getName(), "무광실버");
+        //exception
+        Assertions.assertThrows(ProductNotFoundByNameException.class, () -> cartItemCreateService.createAuthCartItem("authId", imageFile, dto2));
 
-        CustomProduct customProduct1 = cart.getCustomProducts().get(1);
-        Assertions.assertEquals(customProduct1.getQuantity(), 2);
-        Assertions.assertEquals(customProduct1.getProduct().getName(), "Liberty 52_Frame");
+        Assertions.assertThrows(OptionDetailNotFoundByNameException.class, () -> cartItemCreateService.createAuthCartItem("authId", imageFile, dto3));
 
-
-        List<CustomProductOption> productCartOptionList1 = customProduct1.getOptions();
-
-        Assertions.assertEquals(customProduct1.getOptions().size(),3);
-        Assertions.assertEquals(productCartOptionList1.get(0).getOptionDetail().getName(), "벽걸이형");
-        Assertions.assertEquals(productCartOptionList1.get(1).getOptionDetail().getName(), "1mm 두께 승화전사 인쇄용 알루미늄시트");
-        Assertions.assertEquals(productCartOptionList1.get(2).getOptionDetail().getName(), "유광백색");
 
 
     }
