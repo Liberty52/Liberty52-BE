ca1cd70f587b2bd119321ac76881c08ea1f59393
ansm6358 <ansm6358@naver.com>
2023-09-23 18:45:41 +0900

    스케줄러 mock 테스트

--- src/test/java/com/liberty52/product/service/applicationservice/mock/CartItemSchedulerServiceMockTest.java
+++ src/test/java/com/liberty52/product/service/applicationservice/mock/CartItemSchedulerServiceMockTest.java
@@ -1,10 +1,11 @@
-package com.liberty52.product.service.applicationservice;
+package com.liberty52.product.service.applicationservice.mock;
 
 import com.liberty52.product.MockS3Test;
+import com.liberty52.product.service.applicationservice.CartItemCreateService;
+import com.liberty52.product.service.applicationservice.CartItemSchedulerService;
+import com.liberty52.product.service.applicationservice.impl.CartItemSchedulerServiceImpl;
 import com.liberty52.product.service.controller.dto.CartItemRequest;
-import com.liberty52.product.service.entity.Cart;
-import com.liberty52.product.service.entity.CustomProduct;
-import com.liberty52.product.service.entity.CustomProductOption;
+import com.liberty52.product.service.entity.*;
 import com.liberty52.product.service.event.internal.ImageRemovedEvent;
 import com.liberty52.product.service.event.internal.dto.ImageRemovedEventDto;
 import com.liberty52.product.service.repository.CartRepository;
@@ -14,94 +15,124 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest
-@Transactional
-public class CartItemSchedulerServiceTest extends MockS3Test {
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
-    @Autowired
+@ExtendWith(MockitoExtension.class)
+public class CartItemSchedulerServiceMockTest extends MockS3Test {
+
+    @Mock
     CartRepository cartRepository;
 
-    @Autowired
+    @Mock
     CustomProductRepository customProductRepository;
 
-    @Autowired
+    @Mock
     CustomProductOptionRepository customProductOptionRepository;
 
-    @Autowired
-    CartItemCreateService cartItemCreateService;
+    @InjectMocks
+    CartItemSchedulerServiceImpl cartItemSchedulerService;
 
-    @Autowired
-    CartItemSchedulerService cartItemSchedulerService;
+    @Mock
+    ApplicationEventPublisher eventPublisher;
 
-    @Autowired
-    EntityManager entityManager;
-
+    Cart cart;
     List<String> productID = new ArrayList<>();
     List<String> optionId = new ArrayList<>();
     String authId = "guest";
 
     @BeforeEach
-    void init() throws IOException {
-        CartItemRequest dto1 = new CartItemRequest();
-        String[] option1 = {"이젤 거치형", "1mm 두께 승화전사 인쇄용 알루미늄시트", "무광실버"};
-        dto1.create("Liberty 52_Frame", 1, option1);
+    void  init(){
+        Product product = Product.create("Liberty 52_Frame", ProductState.ON_SALE, 100L);
 
-        CartItemRequest dto2 = new CartItemRequest();
-        String[] option2 = {"벽걸이형", "1mm 두께 승화전사 인쇄용 알루미늄시트", "유광백색"};
-        dto2.create("Liberty 52_Frame", 2, option2);
+        ProductOption option1 = ProductOption.create("거치 방식", true, true);
+        option1.associate(product);
+        OptionDetail detailEasel = OptionDetail.create("이젤 거치형", 100,true);
+        detailEasel.associate(option1);
+        OptionDetail detailWall = OptionDetail.create("벽걸이형", 100,true);
+        detailWall.associate(option1);
 
-        MockMultipartFile imageFile = new MockMultipartFile("image", "test.png", "image/jpeg", new FileInputStream("src/test/resources/static/test.jpg"));
+        ProductOption option2 = ProductOption.create("기본소재", true, true);
+        option2.associate(product);
+        OptionDetail material = OptionDetail.create("1mm 두께 승화전사 인쇄용 알루미늄시트", 100,true);
+        material.associate(option2);
 
-        cartItemCreateService.createGuestCartItem(authId, imageFile, dto1);
-        cartItemCreateService.createGuestCartItem(authId, imageFile, dto2);
+        ProductOption option3 = ProductOption.create("기본소재 옵션", true, true);
+        option3.associate(product);
+        OptionDetail materialOption1 = OptionDetail.create("유광실버", 100,true);
+        materialOption1.associate(option3);
+        OptionDetail materialOption2 = OptionDetail.create("무광실버", 100,true);
+        materialOption2.associate(option3);
+        OptionDetail materialOption3 = OptionDetail.create("유광백색", 100,true);
+        materialOption3.associate(option3);
+        OptionDetail materialOption4 = OptionDetail.create("무광백색", 100,true);
+        materialOption4.associate(option3);
 
-        Cart cart = cartRepository.findByAuthId(authId).orElse(null);
+        // Add Cart & CartItems
+        Cart cart = Cart.create("authId");
 
-        for(CustomProduct customProduct : cart.getCustomProducts()){
-            customProduct.getOptions().forEach(option -> optionId.add(option.getId()) );
-            productID.add(customProduct.getId());
-        }
+        CustomProduct customProduct1 = CustomProduct.create("image", 1, "authId");
+        customProduct1.associateWithProduct(product);
+        customProduct1.associateWithCart(cart);
 
-        cart.updateExpiryDate(LocalDate.now().minusDays(7));
-        cartRepository.save(cart);
-        entityManager.flush();
-        entityManager.clear();
+        CustomProductOption customProductOption1 = CustomProductOption.create();
+        customProductOption1.associate(detailEasel);
+        customProductOption1.associate(customProduct1);
+
+        CustomProductOption customProductOption2 = CustomProductOption.create();
+        customProductOption2.associate(material);
+        customProductOption2.associate(customProduct1);
+
+        CustomProductOption customProductOption3 = CustomProductOption.create();
+        customProductOption3.associate(materialOption2);
+        customProductOption3.associate(customProduct1);
+
+        CustomProduct customProduct2 = CustomProduct.create("image", 2, "authId");
+        customProduct2.associateWithProduct(product);
+        customProduct2.associateWithCart(cart);
+
+        CustomProductOption customProductOption4 = CustomProductOption.create();
+        customProductOption4.associate(detailWall);
+        customProductOption4.associate(customProduct2);
+
+        CustomProductOption customProductOption5 = CustomProductOption.create();
+        customProductOption5.associate(materialOption4);
+        customProductOption5.associate(customProduct2);
+        this.cart = cart;
     }
 
     @Test
     void 스케줄러삭제() throws IOException {
-        Cart beforeCart = cartRepository.findByAuthId(authId).orElse(null);
-        Assertions.assertEquals(beforeCart.getAuthId(), authId);
-        cartItemSchedulerService.deleteNonMemberCart();
-        Cart afterCart = cartRepository.findByAuthId(authId).orElse(null);
-        //Cart afterCart = cartRepository.findById(beforeCart.getId()).orElse(null);
-        Assertions.assertNull(afterCart);
-        for(String product : productID){
-            CustomProduct customProduct= customProductRepository.findById(product).orElse(null);
-            Assertions.assertNull(customProduct);
-        }
-        for(String option : optionId){
-            CustomProductOption customProductOption = customProductOptionRepository.findById(option).orElse(null);
-            Assertions.assertNull(customProductOption);
-        }
+        //given
+        List<Cart> carts = new ArrayList<>();
+        carts.add(this.cart);
 
-        //Cart afterCart = cartRepository.findByAuthId(authId).orElse(null);
-        //Assertions.assertNull(afterCart);
+            given(cartRepository.findByExpiryDateLessThan(LocalDate.now())).willReturn(carts);
+        //when
+            cartItemSchedulerService.deleteNonMemberCart();
+        //then
 
-
+        Mockito.verify(cartRepository, atMostOnce()).findByExpiryDateLessThan(any());
     }
 
 }
