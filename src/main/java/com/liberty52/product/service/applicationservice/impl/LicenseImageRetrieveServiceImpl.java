27d7c62b2c58478c4ff6f8564699cf65da75f647
parkcoldroad <coldstreet@naver.com>
2023-10-01 18:24:19 +0900

    feat: retrieveLicenseImages 컨트롤러 추가

--- src/main/java/com/liberty52/product/service/applicationservice/impl/LicenseImageRetrieveServiceImpl.java
+++ src/main/java/com/liberty52/product/service/applicationservice/impl/LicenseImageRetrieveServiceImpl.java
@@ -1,17 +1,12 @@
 package com.liberty52.product.service.applicationservice.impl;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.liberty52.product.global.adapter.s3.S3UploaderApi;
 import com.liberty52.product.global.util.Validator;
-import com.liberty52.product.service.applicationservice.LicenseImageCreateService;
 import com.liberty52.product.service.applicationservice.LicenseImageRetrieveService;
-import com.liberty52.product.service.controller.dto.LicenseImageCreateDto;
 import com.liberty52.product.service.controller.dto.LicenseImageRetrieveDto;
 import com.liberty52.product.service.entity.LicenseImage;
 import com.liberty52.product.service.repository.LicenseImageRepository;
@@ -30,6 +25,6 @@ public class LicenseImageRetrieveServiceImpl implements LicenseImageRetrieveServ
 		List<LicenseImage> licenseImageList = licenseImageRepository.findAll();
 		return licenseImageList.stream()
 			.map(LicenseImageRetrieveDto::new)
-			.collect(Collectors.toList());
+			.toList();
 	}
 }
