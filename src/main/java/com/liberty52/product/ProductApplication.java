6817a0c6f59bff7e6d28a0eac558c0bd842c0c6e
ImKunYoung <lim8667@gmail.com>
2023-10-03 22:19:13 +0900

    [CHORE] 로깅 개선 (#260)
    * chore(#245): Add CRLFLogConverter to sanitize log messages
    
    * perf(#245): Enhance application startup logs with environment details
    
    * perf(#245): Enable Hibernate SQL formatting in application properties.
    
    * perf(#245): Add colorful ASCII art banner for PRODUCT Service.
    
    * perf(#245): Enable ANSI output in Spring configuration.
    
    * feat(#245): Added RequestLoggingInterceptor to Spring Boot application
    
    * refactor(#245): Remove Unused imports
    
    * feat(#245): Modified RequestLoggingInterceptor to log request method, URI, and response status.
    
    * feat(#245): Added afterCompletion method to log response status.
    
    * chore(#245): Adjust Logging Levels in application.properties
    
    * chore(#245): Updated the logging configuration to add 'org.apache.kafka.clients.NetworkClient' with level 'off'.
    
    * chore(#245): Updated the logging configuration to add 'aws.ConfigClusterResolver' with level 'off'.

--- src/main/java/com/liberty52/product/ProductApplication.java
+++ src/main/java/com/liberty52/product/ProductApplication.java
@@ -1,24 +1,74 @@
 package com.liberty52.product;
 
-import jakarta.annotation.PostConstruct;
+import com.liberty52.product.global.config.CRLFLogConverter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.TimeZone;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
 
 @EnableAsync
 @EnableScheduling
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@Slf4j
 public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
+        SpringApplication app = new SpringApplication(ProductApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
 	}
 
+    private static void logApplicationStartup(Environment env) {
+        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
+        String serverPort = env.getProperty("server.port");
+        String contextPath = Optional
+                .ofNullable(env.getProperty("server.servlet.context-path"))
+                .filter(StringUtils::isNotBlank)
+                .orElse("/");
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info(
+                CRLFLogConverter.CRLF_SAFE_MARKER,
+                "\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
+        );
+
+        String configServerStatus = env.getProperty("configserver.status");
+        if (configServerStatus == null) {
+            configServerStatus = "Not found or not setup for this application";
+        }
+        log.info(
+                CRLFLogConverter.CRLF_SAFE_MARKER,
+                "\n----------------------------------------------------------\n\t" +
+                        "Config Server: \t{}\n----------------------------------------------------------",
+                configServerStatus
+        );
+    }
 }
