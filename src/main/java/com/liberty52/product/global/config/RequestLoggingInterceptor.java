8897f58c8d0ef49aa2228a7c3d51879befcd98c1
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-03 19:17:33 +0900

    feat(#245): Added RequestLoggingInterceptor to Spring Boot application

---
+++ src/main/java/com/liberty52/product/global/config/RequestLoggingInterceptor.java
@@ -0,0 +1,20 @@
+package com.liberty52.product.global.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.logging.Logger;
+
+@Component
+@Slf4j
+public class RequestLoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("Request URI: " + request.getRequestURI());
+        return true;
+    }
+}
--- src/main/java/com/liberty52/product/global/config/WebConfig.java
+++ src/main/java/com/liberty52/product/global/config/WebConfig.java
@@ -3,12 +3,21 @@ package com.liberty52.product.global.config;
 import com.liberty52.product.global.constants.TranslationConstants;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new TranslationConstants.LangCodeConverter());
     }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestLoggingInterceptor());
+    }
 }
+
