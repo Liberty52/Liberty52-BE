a08578b6c2765df65cb1ec5175ab5f7e447bf013
parkcoldroad <coldstreet@naver.com>
2023-10-04 10:38:58 +0900

    chore: Make JacksonConfig from feign to transmit dto and MultipartFile

--- src/main/java/com/liberty52/product/service/controller/LicenseImageCreateController.java
+++ src/main/java/com/liberty52/product/service/controller/LicenseImageCreateController.java
@@ -1,7 +1,6 @@
 package com.liberty52.product.service.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -9,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.liberty52.product.service.applicationservice.LicenseImageCreateService;
 import com.liberty52.product.service.controller.dto.LicenseImageCreateDto;
 
@@ -18,12 +19,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LicenseImageCreateController {
 	private final LicenseImageCreateService licenseImageCreateService;
+	private final ObjectMapper objectMapper;
 
 	@PostMapping("/admin/licenseImage")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createLicenseImage(@RequestHeader("LB-Role") String role,
-		@RequestPart("dto") @Validated LicenseImageCreateDto dto,
-		@RequestPart(value = "image") MultipartFile licenseImageFile) {
+		@RequestPart("dto") String dtoJson,
+		@RequestPart(value = "image") MultipartFile licenseImageFile) throws JsonProcessingException {
+		LicenseImageCreateDto dto = objectMapper.readValue(dtoJson, LicenseImageCreateDto.class);
 		licenseImageCreateService.createLicenseImage(role, dto, licenseImageFile);
 	}
 }
