faf0f9acf198febfebf484332b9ec07f5d472406
parkcoldroad <coldstreet@naver.com>
2023-10-04 22:29:59 +0900

    hotfix: jacksonConfig 삭제

--- src/main/java/com/liberty52/product/service/controller/LicenseImageCreateController.java
+++ src/main/java/com/liberty52/product/service/controller/LicenseImageCreateController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.liberty52.product.service.applicationservice.LicenseImageCreateService;
 import com.liberty52.product.service.controller.dto.LicenseImageCreateDto;
 
@@ -19,14 +17,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LicenseImageCreateController {
 	private final LicenseImageCreateService licenseImageCreateService;
-	private final ObjectMapper objectMapper;
 
 	@PostMapping("/admin/licenseImage")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createLicenseImage(@RequestHeader("LB-Role") String role,
-		@RequestPart("dto") String dtoJson,
-		@RequestPart(value = "image") MultipartFile licenseImageFile) throws JsonProcessingException {
-		LicenseImageCreateDto dto = objectMapper.readValue(dtoJson, LicenseImageCreateDto.class);
+		@RequestPart("dto") LicenseImageCreateDto dto,
+		@RequestPart(value = "image") MultipartFile licenseImageFile) {
 		licenseImageCreateService.createLicenseImage(role, dto, licenseImageFile);
 	}
 }
