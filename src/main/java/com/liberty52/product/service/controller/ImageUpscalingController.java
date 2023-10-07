324a9994a0229c47f437784192aee35207e5e7ce
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 13:41:59 +0900

    docs(#244): Add Swagger documentation to ImageUpscalingController

--- src/main/java/com/liberty52/product/service/controller/ImageUpscalingController.java
+++ src/main/java/com/liberty52/product/service/controller/ImageUpscalingController.java
@@ -2,6 +2,8 @@ package com.liberty52.product.service.controller;
 
 import com.liberty52.product.service.applicationservice.ImageUpscalingService;
 import com.liberty52.product.service.controller.dto.ImageUpscalingDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "이미지", description = "이미지 관련 API를 제공합니다")
 public class ImageUpscalingController {
     private final ImageUpscalingService service;
 
     @PostMapping("/images/upscaling")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "이미지 업스케일링", description = "이미지를 업스케일링합니다.")
     public ImageUpscalingDto.Response generateImage(
         @Validated @RequestBody ImageUpscalingDto.Request requestBody
     ) {
