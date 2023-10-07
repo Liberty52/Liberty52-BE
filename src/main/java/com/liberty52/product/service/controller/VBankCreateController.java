420b9648cfca419f07dee409cd22a83ea2128c21
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 15:56:59 +0900

    docs(#244): Add Swagger documentation to 가상계좌

--- src/main/java/com/liberty52/product/service/controller/VBankCreateController.java
+++ src/main/java/com/liberty52/product/service/controller/VBankCreateController.java
@@ -2,6 +2,7 @@ package com.liberty52.product.service.controller;
 
 import com.liberty52.product.service.applicationservice.VBankCreateService;
 import com.liberty52.product.service.controller.dto.VBankCreate;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,13 +10,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "결제", description = "결제 관련 API를 제공합니다")
+@Tag(name = "가상계좌", description = "가상계좌 관련 API를 제공합니다")
 @RestController
 @RequiredArgsConstructor
 public class VBankCreateController {
 
     private final VBankCreateService vBankCreateService;
 
+    @Operation(summary = "가상계좌 생성", description = "관리자가 가상계좌를 생성합니다.")
     @PostMapping("/admin/vbanks")
     @ResponseStatus(HttpStatus.CREATED)
     public VBankCreate.Response createVBankByAdmin(@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
--- src/main/java/com/liberty52/product/service/controller/VBankRetrieveController.java
+++ src/main/java/com/liberty52/product/service/controller/VBankRetrieveController.java
@@ -2,6 +2,7 @@ package com.liberty52.product.service.controller;
 
 import com.liberty52.product.service.applicationservice.VBankRetrieveService;
 import com.liberty52.product.service.controller.dto.VBankRetrieve;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "결제", description = "결제 관련 API를 제공합니다")
+@Tag(name = "가상계좌", description = "가상계좌 관련 API를 제공합니다")
 @RestController
 @RequiredArgsConstructor
 public class VBankRetrieveController {
 
     private final VBankRetrieveService vBankRetrieveService;
 
+    @Operation(summary = "가상계좌 목록 조회", description = "등록된 가상계좌 목록을 조회합니다.")
     @GetMapping("/vbanks")
     @ResponseStatus(HttpStatus.OK)
     public VBankRetrieve.ListResponse getVBankInfoList() {
