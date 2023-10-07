9081ed19b8b0baf498d90affb0657cd1baaf1ba9
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 15:56:38 +0900

    docs(#244): Add Swagger documentation to 번역

--- src/main/java/com/liberty52/product/service/controller/TranslationController.java
+++ src/main/java/com/liberty52/product/service/controller/TranslationController.java
@@ -1,18 +1,22 @@
 package com.liberty52.product.service.controller;
 
-import com.liberty52.product.service.applicationservice.TranslationService;
-import com.liberty52.product.service.controller.dto.TranslationDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import com.liberty52.product.service.applicationservice.TranslationService;
+import com.liberty52.product.service.controller.dto.TranslationDto;
 
+@Tag(name = "번역", description = "텍스트 번역 API를 제공합니다")
 @RestController
 @RequiredArgsConstructor
 public class TranslationController {
     private final TranslationService translationService;
 
+    @Operation(summary = "텍스트 번역", description = "입력된 텍스트를 번역하여 응답합니다.")
     @PostMapping("/texts/translations")
     @ResponseStatus(HttpStatus.CREATED)
     public TranslationDto.Response translateText(@RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
