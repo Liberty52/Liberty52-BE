4ad5660f98a156a91dc332ceba569f6716b791f6
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 12:52:31 +0900

    docs(#244): Add Swagger documentation to CartItemCreateController

--- src/main/java/com/liberty52/product/service/controller/CartItemCreateController.java
+++ src/main/java/com/liberty52/product/service/controller/CartItemCreateController.java
@@ -2,6 +2,8 @@ package com.liberty52.product.service.controller;
 
 import com.liberty52.product.service.applicationservice.CartItemCreateService;
 import com.liberty52.product.service.controller.dto.CartItemRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,11 +12,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "장바구니", description = "장바구니 관련 API를 제공합니다.")
 public class CartItemCreateController {
     private final CartItemCreateService cartItemCreateService;
 
     @PostMapping("/carts/custom-products")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "비회원 장바구니 상품 생성", description = "주어진 인증 ID, 이미지 파일 및 장바구니 상품 요청을 사용하여 비회원 장바구니 상품을 생성합니다.")
     public void createAuthCartItem(@RequestHeader(HttpHeaders.AUTHORIZATION) String authId, @RequestPart(value = "file") MultipartFile imageFile, @RequestPart CartItemRequest dto) {
         cartItemCreateService.createAuthCartItem(authId, imageFile, dto);
     }
