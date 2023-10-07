9f907e4ca07481716b01016148cc552ef818e0a3
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 13:43:15 +0900

    docs(#244): Add Swagger documentation to OrderCancelController

--- src/main/java/com/liberty52/product/service/controller/OrderCancelController.java
+++ src/main/java/com/liberty52/product/service/controller/OrderCancelController.java
@@ -3,6 +3,8 @@ package com.liberty52.product.service.controller;
 import com.liberty52.product.service.applicationservice.OrderCancelService;
 import com.liberty52.product.service.controller.dto.OrderCancelDto;
 import com.liberty52.product.service.controller.dto.OrderRefundDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "주문", description = "주문 관련 API를 제공합니다")
 public class OrderCancelController {
 
     private final OrderCancelService orderCancelService;
 
     @PostMapping("/orders/cancel")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "주문 취소", description = "사용자가 주문을 취소합니다.")
     public OrderCancelDto.Response cancelOrder(@RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
                                                @RequestBody @Validated OrderCancelDto.Request request) {
         return orderCancelService.cancelOrder(authId, request);
@@ -24,6 +28,7 @@ public class OrderCancelController {
 
     @PostMapping("/admin/orders/refund")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "주문 환불", description = "관리자가 고객의 주문을 환불합니다.")
     public void refundCustomerOrderByAdmin(@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
                                     @RequestHeader("LB-Role") String role,
                                     @RequestBody @Validated OrderRefundDto.Request request) {
