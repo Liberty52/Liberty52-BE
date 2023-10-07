df1a9cdb0f01dee22e2894123628b985207b61db
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 13:42:53 +0900

    docs(#244): Add Swagger documentation to LicenseImageRetrieveController

--- src/main/java/com/liberty52/product/service/controller/LicenseImageRetrieveController.java
+++ src/main/java/com/liberty52/product/service/controller/LicenseImageRetrieveController.java
@@ -2,6 +2,8 @@ package com.liberty52.product.service.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,11 +17,13 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "이미지", description = "이미지 관련 API를 제공합니다")
 public class LicenseImageRetrieveController {
 	private final LicenseImageRetrieveService licenseImageRetrieveService;
 
 	@GetMapping("/admin/licenseImage")
 	@ResponseStatus(HttpStatus.OK)
+	@Operation(summary = "라이선스 이미지 조회", description = "라이선스 이미지를 조회합니다.")
 	public List<LicenseImageRetrieveDto> retrieveLicenseImages(@RequestHeader("LB-Role") String role) {
 		return licenseImageRetrieveService.retrieveLicenseImages(role);
 	}
