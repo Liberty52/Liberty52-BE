1affef3b8189789aad6bc6d7b294d4e810903e2e
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 13:41:36 +0900

    docs(#244): Add Swagger documentation to DeliveryOptionModifyController

--- src/main/java/com/liberty52/product/service/controller/DeliveryOptionModifyController.java
+++ src/main/java/com/liberty52/product/service/controller/DeliveryOptionModifyController.java
@@ -2,6 +2,8 @@ package com.liberty52.product.service.controller;
 
 import com.liberty52.product.service.applicationservice.DeliveryOptionModifyService;
 import com.liberty52.product.service.controller.dto.DeliveryOptionFeeModify;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "배송 정보", description = "배송 정보 관련 API를 제공합니다.")
 public class DeliveryOptionModifyController {
 
     private final DeliveryOptionModifyService deliveryOptionModifyService;
 
     @PatchMapping("/admin/options/delivery/fee")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "기본 배송비 수정", description = "관리자 권한을 사용하여 기본 배송비를 수정합니다.")
     public DeliveryOptionFeeModify.Response updateDefaultDeliveryFeeByAdmin(@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
                                                                             @RequestHeader("LB-Role") String role,
                                                                             @RequestBody @Valid DeliveryOptionFeeModify.Request request) {
