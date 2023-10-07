598288fe1d718deb82096ea220e028e127557c73
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 13:50:56 +0900

    docs(#244): Update Swagger documentation to OrderCancelController
    docs(#244): Update Swagger documentation to OrderCancelController

--- src/main/java/com/liberty52/product/service/controller/OrderCancelController.java
+++ src/main/java/com/liberty52/product/service/controller/OrderCancelController.java
@@ -1,15 +1,17 @@
 package com.liberty52.product.service.controller;
 
-import com.liberty52.product.service.applicationservice.OrderCancelService;
-import com.liberty52.product.service.controller.dto.OrderCancelDto;
-import com.liberty52.product.service.controller.dto.OrderRefundDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import com.liberty52.product.service.applicationservice.OrderCancelService;
+import com.liberty52.product.service.controller.dto.OrderCancelDto;
+import com.liberty52.product.service.controller.dto.OrderRefundDto;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,21 +20,24 @@ public class OrderCancelController {
 
     private final OrderCancelService orderCancelService;
 
+    @Operation(summary = "주문 취소", description = "사용자가 주문을 취소하는 엔드포인트입니다.")
     @PostMapping("/orders/cancel")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "주문 취소", description = "사용자가 주문을 취소합니다.")
-    public OrderCancelDto.Response cancelOrder(@RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
-                                               @RequestBody @Validated OrderCancelDto.Request request) {
+    public OrderCancelDto.Response cancelOrder(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
+            @RequestBody @Validated OrderCancelDto.Request request
+    ) {
         return orderCancelService.cancelOrder(authId, request);
     }
 
+    @Operation(summary = "고객 환불 주문 생성", description = "관리자가 고객에 대한 환불 주문을 생성하는 엔드포인트입니다.")
     @PostMapping("/admin/orders/refund")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @Operation(summary = "주문 환불", description = "관리자가 고객의 주문을 환불합니다.")
-    public void refundCustomerOrderByAdmin(@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
-                                    @RequestHeader("LB-Role") String role,
-                                    @RequestBody @Validated OrderRefundDto.Request request) {
+    public void refundCustomerOrderByAdmin(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
+            @RequestHeader("LB-Role") String role,
+            @RequestBody @Validated OrderRefundDto.Request request
+    ) {
         orderCancelService.refundCustomerOrderByAdmin(adminId, role, request);
     }
-
 }
