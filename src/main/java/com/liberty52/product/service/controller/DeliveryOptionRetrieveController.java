955f4070703df70bacb2613134d3f201bcc8a44d
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 13:41:45 +0900

    docs(#244): Add Swagger documentation to DeliveryOptionRetrieveController

--- src/main/java/com/liberty52/product/service/controller/DeliveryOptionRetrieveController.java
+++ src/main/java/com/liberty52/product/service/controller/DeliveryOptionRetrieveController.java
@@ -2,6 +2,8 @@ package com.liberty52.product.service.controller;
 
 import com.liberty52.product.service.applicationservice.DeliveryOptionRetrieveService;
 import com.liberty52.product.service.controller.dto.DeliveryOptionFeeRetrieve;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "배송 정보", description = "배송 정보 관련 API를 제공합니다.")
 public class DeliveryOptionRetrieveController {
 
     private final DeliveryOptionRetrieveService deliveryOptionRetrieveService;
 
     @GetMapping("/options/delivery/fee")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "기본 배송비 조회", description = "기본 배송비를 조회합니다.")
     public DeliveryOptionFeeRetrieve.Response getDefaultDeliveryFee() {
         return DeliveryOptionFeeRetrieve.Response.fromDto(
                 deliveryOptionRetrieveService.getDefaultDeliveryFee()
