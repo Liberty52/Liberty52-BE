f473bd9f96e7648840b982cd652b7d0b0e527d73
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-07 13:50:45 +0900

    docs(#244): Update Swagger documentation to OrderCreateController

--- src/main/java/com/liberty52/product/service/controller/OrderCreateController.java
+++ src/main/java/com/liberty52/product/service/controller/OrderCreateController.java
@@ -17,9 +17,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class OrderCreateController {
     private final OrderCreateService orderCreateService;
 
+    @Operation(summary = "카드 결제 주문 생성", description = "카드 결제 주문을 생성하는 엔드포인트입니다.")
     @PostMapping("/orders/card")
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "카드 결제 주문 생성", description = "카드 결제 주문을 생성하는 엔드포인트입니다.")
     public PaymentCardResponseDto createCardPaymentOrders(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
             @RequestPart("dto") @Validated OrderCreateRequestDto dto,
@@ -28,9 +28,9 @@ public class OrderCreateController {
         return orderCreateService.createCardPaymentOrders(authId, dto, imageFile);
     }
 
+    @Operation(summary = "카드 결제 주문 최종 승인 확인", description = "카드 결제 주문의 최종 승인을 확인하는 엔드포인트입니다.")
     @GetMapping("/orders/card/{orderId}/confirm")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "카드 결제 주문 최종 승인 확인", description = "카드 결제 주문의 최종 승인을 확인하는 엔드포인트입니다.")
     public PaymentConfirmResponseDto confirmFinalApprovalOfCardPayment(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
             @PathVariable("orderId") String orderId
@@ -38,9 +38,9 @@ public class OrderCreateController {
         return orderCreateService.confirmFinalApprovalOfCardPayment(authId, orderId);
     }
 
+    @Operation(summary = "가상계좌 결제 주문 생성", description = "사용자가 가상계좌 결제 주문을 생성하는 엔드포인트입니다.")
     @PostMapping("/orders/vbank")
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "가상계좌 결제 주문 생성", description = "사용자가 가상계좌 결제 주문을 생성하는 엔드포인트입니다.")
     public PaymentVBankResponseDto createVBankPaymentOrders(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
             @RequestPart("dto") @Validated OrderCreateRequestDto dto,
@@ -49,9 +49,9 @@ public class OrderCreateController {
         return orderCreateService.createVBankPaymentOrders(authId, dto, imageFile);
     }
 
+    @Operation(summary = "카트를 이용한 카드 결제 주문 생성", description = "카트를 이용하여 카드 결제 주문을 생성하는 엔드포인트입니다.")
     @PostMapping("/orders/card/carts")
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "카트를 이용한 카드 결제 주문 생성", description = "카트를 이용하여 카드 결제 주문을 생성하는 엔드포인트입니다.")
     public PaymentCardResponseDto createCardPaymentOrdersByCarts(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
             @RequestBody @Validated OrderCreateRequestDto dto
@@ -59,9 +59,9 @@ public class OrderCreateController {
         return orderCreateService.createCardPaymentOrdersByCarts(authId, dto);
     }
 
+    @Operation(summary = "카트를 이용한 가상 계좌 결제 주문 생성", description = "카트를 이용하여 가상 계좌 결제 주문을 생성하는 엔드포인트입니다.")
     @PostMapping("/orders/vbank/carts")
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "카트를 이용한 가상 계좌 결제 주문 생성", description = "카트를 이용하여 가상 계좌 결제 주문을 생성하는 엔드포인트입니다.")
     public PaymentVBankResponseDto createVBankPaymentOrdersByCarts(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authId,
             @RequestBody @Validated OrderCreateRequestDto dto
