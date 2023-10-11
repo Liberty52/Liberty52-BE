2a2ab04c4b2d6251adcca7d946ef99cd05268a34
parkcoldroad <coldstreet@naver.com>
2023-10-11 14:55:53 +0900

    feat: 라이센스 이미지 상세 조회 추가

--- src/main/java/com/liberty52/product/service/applicationservice/LicenseImageRetrieveService.java
+++ src/main/java/com/liberty52/product/service/applicationservice/LicenseImageRetrieveService.java
@@ -8,5 +8,8 @@ import com.liberty52.product.service.controller.dto.LicenseImageRetrieveDto;
 public interface LicenseImageRetrieveService {
 	List<LicenseImageRetrieveByAdminDto> retrieveLicenseImagesByAdmin(String role);
 
+	LicenseImageRetrieveByAdminDto retrieveLicenseImageDetailsByAdmin(String role, String licenseImageId);
+
 	List<LicenseImageRetrieveDto> retrieveLicenseImages();
+
 }
