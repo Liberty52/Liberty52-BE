20aff75117ce67e23c6660ef7be6e66787db51e5
parkcoldroad <coldstreet@naver.com>
2023-10-24 13:49:56 +0900

    feat: dto에 기본 생성자 추가

--- src/main/java/com/liberty52/product/service/applicationservice/impl/ProductIntroductionDeleteServiceImpl.java
+++ src/main/java/com/liberty52/product/service/applicationservice/impl/ProductIntroductionDeleteServiceImpl.java
@@ -2,14 +2,10 @@ package com.liberty52.product.service.applicationservice.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.liberty52.product.global.adapter.s3.S3UploaderApi;
-import com.liberty52.product.global.exception.external.badrequest.BadRequestException;
 import com.liberty52.product.global.exception.external.notfound.ResourceNotFoundException;
 import com.liberty52.product.global.util.Validator;
 import com.liberty52.product.service.applicationservice.ProductIntroductionDeleteService;
-import com.liberty52.product.service.applicationservice.ProductIntroductionUpsertService;
 import com.liberty52.product.service.entity.Product;
 import com.liberty52.product.service.repository.ProductRepository;
 
