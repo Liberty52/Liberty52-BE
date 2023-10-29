96da1741813e1014c9f98a9f5cbfd192289950b9
parkcoldroad <coldstreet@naver.com>
2023-10-29 21:15:00 +0900

    test: 라이선스 옵션 디테일 생성,수정 테스트 추가

---
+++ src/test/java/com/liberty52/product/service/applicationservice/mock/LicenseOptionDetailCreateMockTest.java
@@ -0,0 +1,117 @@
+package com.liberty52.product.service.applicationservice.mock;
+
+import static com.liberty52.product.global.constants.RoleConstants.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.liberty52.product.global.adapter.s3.S3UploaderApi;
+import com.liberty52.product.global.exception.external.badrequest.BadRequestException;
+import com.liberty52.product.service.applicationservice.impl.LicenseOptionDetailCreateServiceImpl;
+import com.liberty52.product.service.controller.dto.LicenseOptionDetailCreateDto;
+import com.liberty52.product.service.entity.license.LicenseOption;
+import com.liberty52.product.service.entity.license.LicenseOptionDetail;
+import com.liberty52.product.service.repository.LicenseOptionDetailRepository;
+import com.liberty52.product.service.repository.LicenseOptionRepository;
+
+@ExtendWith(MockitoExtension.class)
+class LicenseOptionDetailCreateMockTest {
+
+	@InjectMocks
+	LicenseOptionDetailCreateServiceImpl licenseOptionDetailCreateService;
+
+	@Mock
+	LicenseOptionRepository licenseOptionRepository;
+
+	@Mock
+	LicenseOptionDetailRepository licenseOptionDetailRepository;
+
+	@Mock
+	S3UploaderApi s3Uploader;
+
+	@Test
+	void createLicenseOptionDetailByAdminTest() {
+		// Given
+		String licenseOptionId = "testLicenseOptionId";
+		LicenseOptionDetailCreateDto dto = LicenseOptionDetailCreateDto.create
+			("testArtName", "testArtistName", 100, true);
+		MultipartFile artImageFile = mock(MultipartFile.class);
+		LicenseOption mockLicenseOption = mock(LicenseOption.class);
+
+		when(licenseOptionRepository.findById(licenseOptionId)).thenReturn(Optional.of(mockLicenseOption));
+		when(s3Uploader.upload(artImageFile)).thenReturn("testArtUrl");
+
+		// When
+		licenseOptionDetailCreateService.createLicenseOptionDetailByAdmin(ADMIN, dto, licenseOptionId, artImageFile);
+
+		// Then
+		ArgumentCaptor<LicenseOptionDetail> captor = ArgumentCaptor.forClass(LicenseOptionDetail.class);
+		verify(licenseOptionDetailRepository, times(1)).save(captor.capture());
+		LicenseOptionDetail savedLicenseOptionDetail = captor.getValue();
+		assertEquals(dto.getArtName(), savedLicenseOptionDetail.getArtName());
+		assertEquals(dto.getArtistName(), savedLicenseOptionDetail.getArtistName());
+		assertEquals(dto.getStock(), savedLicenseOptionDetail.getStock());
+		assertEquals(dto.getOnSale(), savedLicenseOptionDetail.getOnSale());
+		assertEquals("testArtUrl", savedLicenseOptionDetail.getArtUrl());
+	}
+
+	@Test
+	void createLicenseOptionDetailByAdmin_WhenLicenseOptionNotFound_ThrowException() {
+		// Given
+		String licenseOptionId = "testLicenseOptionId";
+		LicenseOptionDetailCreateDto dto = LicenseOptionDetailCreateDto.create
+			("testArtName", "testArtistName", 100, true);
+		MultipartFile artImageFile = mock(MultipartFile.class);
+
+		when(licenseOptionRepository.findById(licenseOptionId)).thenReturn(Optional.empty());
+
+		// When
+		// Then
+		assertThrows(RuntimeException.class, () -> {
+			licenseOptionDetailCreateService.createLicenseOptionDetailByAdmin(ADMIN, dto, licenseOptionId,
+				artImageFile);
+		});
+	}
+
+	@Test
+	void createLicenseOptionDetailByAdmin_WhenRoleIsNotAdmin() {
+		// Given
+		String licenseOptionId = "testLicenseOptionId";
+		LicenseOptionDetailCreateDto dto = LicenseOptionDetailCreateDto.create
+			("testArtName", "testArtistName", 100, true);
+		MultipartFile artImageFile = mock(MultipartFile.class);
+
+		// When
+		// Then
+		assertThrows(RuntimeException.class, () -> {
+			licenseOptionDetailCreateService.createLicenseOptionDetailByAdmin("USER", dto, licenseOptionId,
+				artImageFile);
+		});
+	}
+
+	@Test
+	void createLicenseOptionDetailByAdmin_WhenArtImageFileIsNull() {
+		// Given
+		String licenseOptionId = "testLicenseOptionId";
+		LicenseOptionDetailCreateDto dto = LicenseOptionDetailCreateDto.create
+			("testArtName", "testArtistName", 100, true);
+		MultipartFile artImageFile = null;
+		LicenseOption mockLicenseOption = mock(LicenseOption.class);
+
+		when(licenseOptionRepository.findById(licenseOptionId)).thenReturn(Optional.of(mockLicenseOption));
+		// When
+		// Then
+		assertThrows(BadRequestException.class,
+			() -> licenseOptionDetailCreateService.createLicenseOptionDetailByAdmin(ADMIN, dto, licenseOptionId, artImageFile));
+
+	}
+}
---
+++ src/test/java/com/liberty52/product/service/applicationservice/mock/LicenseOptionDetailModifyMockTest.java
@@ -0,0 +1,120 @@
+package com.liberty52.product.service.applicationservice.mock;
+
+import static com.liberty52.product.global.constants.RoleConstants.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.liberty52.product.global.adapter.s3.S3UploaderApi;
+import com.liberty52.product.global.exception.external.badrequest.BadRequestException;
+import com.liberty52.product.global.exception.external.notfound.ResourceNotFoundException;
+import com.liberty52.product.service.applicationservice.impl.LicenseOptionDetailModifyServiceImpl;
+import com.liberty52.product.service.controller.dto.LicenseOptionDetailModifyDto;
+import com.liberty52.product.service.entity.license.LicenseOptionDetail;
+import com.liberty52.product.service.repository.LicenseOptionDetailRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class LicenseOptionDetailModifyMockTest {
+
+	@InjectMocks
+	LicenseOptionDetailModifyServiceImpl licenseOptionDetailModifyService;
+
+	@Mock
+	LicenseOptionDetailRepository licenseOptionDetailRepository;
+
+	@Mock
+	S3UploaderApi s3Uploader;
+
+	@Test
+	void modifyLicenseOptionDetailByAdminTest() {
+		// Given
+		String licenseOptionDetailId = "testLicenseOptionDetailId";
+		LicenseOptionDetailModifyDto dto = new LicenseOptionDetailModifyDto("ArtName", "ArtistName", 10, true);
+		MultipartFile artImageFile = mock(MultipartFile.class);
+
+		LicenseOptionDetail mockLicenseOptionDetail = mock(LicenseOptionDetail.class);
+		when(licenseOptionDetailRepository.findById(licenseOptionDetailId)).thenReturn(
+			Optional.of(mockLicenseOptionDetail));
+		when(s3Uploader.upload(artImageFile)).thenReturn("modifiedArtUrl");
+
+		// When
+		licenseOptionDetailModifyService.modifyLicenseOptionDetailByAdmin(ADMIN, licenseOptionDetailId, dto,
+			artImageFile);
+
+		// Then
+		verify(mockLicenseOptionDetail, times(1)).modifyLicenseOptionDetail(dto, "modifiedArtUrl");
+	}
+
+	@Test
+	void modifyLicenseOptionDetailByAdmin_When_LicenseOptionDetailNotFoundTest() {
+		// Given
+		String licenseOptionDetailId = "testLicenseOptionDetailId";
+		LicenseOptionDetailModifyDto dto = new LicenseOptionDetailModifyDto("ArtName", "ArtistName", 10, true);
+		MultipartFile artImageFile = mock(MultipartFile.class);
+
+		when(licenseOptionDetailRepository.findById(licenseOptionDetailId)).thenReturn(Optional.empty());
+
+		assertThrows(
+			ResourceNotFoundException.class,
+			() -> licenseOptionDetailModifyService.modifyLicenseOptionDetailByAdmin(ADMIN, licenseOptionDetailId, dto,
+				artImageFile));
+	}
+
+	@Test
+	void modifyLicenseOptionDetailByAdmin_When_ArtImageFileIsNullTest() {
+		// Given
+		String licenseOptionDetailId = "testLicenseOptionDetailId";
+		LicenseOptionDetailModifyDto dto = new LicenseOptionDetailModifyDto("ArtName", "ArtistName", 10, true);
+		MultipartFile artImageFile = null;
+
+		LicenseOptionDetail mockLicenseOptionDetail = mock(LicenseOptionDetail.class);
+		when(licenseOptionDetailRepository.findById(licenseOptionDetailId)).thenReturn(
+			Optional.of(mockLicenseOptionDetail));
+
+		// When
+		// Then
+		assertThrows(BadRequestException.class,
+			() -> licenseOptionDetailModifyService.modifyLicenseOptionDetailByAdmin(ADMIN, licenseOptionDetailId, dto,
+				artImageFile));
+	}
+
+	@Test
+	void modifyLicenseOptionDetailOnSailStateByAdminTest() {
+		// Given
+		String licenseOptionDetailId = "testLicenseOptionDetailId";
+
+		LicenseOptionDetail mockLicenseOptionDetail = mock(LicenseOptionDetail.class);
+		when(licenseOptionDetailRepository.findById(licenseOptionDetailId)).thenReturn(
+			Optional.of(mockLicenseOptionDetail));
+
+		// When
+		licenseOptionDetailModifyService.modifyLicenseOptionDetailOnSailStateByAdmin(ADMIN, licenseOptionDetailId);
+
+		// Then
+		verify(mockLicenseOptionDetail, times(1)).updateOnSale();
+	}
+
+	@Test
+	void modifyLicenseOptionDetailOnSailStateByAdmin_When_LicenseOptionDetailNotFoundTest() {
+		// Given
+		String licenseOptionDetailId = "testLicenseOptionDetailId";
+
+		when(licenseOptionDetailRepository.findById(licenseOptionDetailId)).thenReturn(Optional.empty());
+
+		// When
+		// Then
+		assertThrows(
+			ResourceNotFoundException.class,
+			() -> licenseOptionDetailModifyService.modifyLicenseOptionDetailOnSailStateByAdmin(ADMIN,
+				licenseOptionDetailId));
+	}
+}
