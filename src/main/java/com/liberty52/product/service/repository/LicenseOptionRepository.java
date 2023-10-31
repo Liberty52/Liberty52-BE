6b8255e94f899fdd3f859e1a3c9265e761c9a735
Changil Park <55132026+parkcoldroad@users.noreply.github.com>
2023-11-01 00:49:01 +0900

    fix: 상품, 라이선스 상품 옵션 연관 관계 해결 (#344)

--- src/main/java/com/liberty52/product/service/repository/LicenseOptionRepository.java
+++ src/main/java/com/liberty52/product/service/repository/LicenseOptionRepository.java
@@ -1,8 +1,11 @@
 package com.liberty52.product.service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.liberty52.product.service.entity.license.LicenseOption;
 
 public interface LicenseOptionRepository extends JpaRepository<LicenseOption, String> {
+	Optional<LicenseOption> findByName(String name);
 }
