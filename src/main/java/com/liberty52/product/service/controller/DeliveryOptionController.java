42e0ea080dc11e7b30dfc044e417ed435a80bf43
IMKUNYOUNG <lim8667@gmail.com>
2023-11-13 02:24:27 +0000

    refactor: Separate DeliveryOptionController based on the Actor.

--- src/main/java/com/liberty52/product/service/controller/DeliveryOptionController.java
+++ src/main/java/com/liberty52/product/service/controller/DeliveryOptionController.java
@@ -1,23 +1,14 @@
 package com.liberty52.product.service.controller;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.liberty52.product.service.applicationservice.DeliveryOptionModifyService;
 import com.liberty52.product.service.applicationservice.DeliveryOptionRetrieveService;
-import com.liberty52.product.service.controller.dto.DeliveryOptionFeeModify;
 import com.liberty52.product.service.controller.dto.DeliveryOptionFeeRetrieve;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "배송", description = "배송 관련 API를 제공합니다.")
 @RestController
@@ -25,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 public class DeliveryOptionController {
 
 	private final DeliveryOptionRetrieveService deliveryOptionRetrieveService;
-	private final DeliveryOptionModifyService deliveryOptionModifyService;
 
 	@Operation(summary = "기본 배송비 조회", description = "기본 배송비를 조회합니다.")
 	@GetMapping("/options/delivery/fee")
@@ -36,15 +26,4 @@ public class DeliveryOptionController {
 		);
 	}
 
-	@Operation(summary = "기본 배송비 수정", description = "관리자 권한을 사용하여 기본 배송비를 수정합니다.")
-	@PatchMapping("/admin/options/delivery/fee")
-	@ResponseStatus(HttpStatus.OK)
-	public DeliveryOptionFeeModify.Response updateDefaultDeliveryFeeByAdmin(
-		@RequestHeader(HttpHeaders.AUTHORIZATION) String adminId,
-		@RequestHeader("LB-Role") String role,
-		@RequestBody @Valid DeliveryOptionFeeModify.Request request) {
-		return DeliveryOptionFeeModify.Response.fromDto(
-			deliveryOptionModifyService.updateDefaultDeliveryFeeByAdmin(role, request.getFee())
-		);
-	}
 }
--- src/test/java/com/liberty52/product/service/controller/DeliveryOptionControllerTest.java
+++ src/test/java/com/liberty52/product/service/controller/DeliveryOptionControllerTest.java
@@ -1,41 +1,30 @@
 package com.liberty52.product.service.controller;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.time.LocalDateTime;
-
+import com.liberty52.product.service.applicationservice.impl.DeliveryOptionRetrieveServiceImpl;
+import com.liberty52.product.service.controller.dto.DeliveryOptionDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.liberty52.product.service.applicationservice.impl.DeliveryOptionModifyServiceImpl;
-import com.liberty52.product.service.applicationservice.impl.DeliveryOptionRetrieveServiceImpl;
-import com.liberty52.product.service.controller.dto.DeliveryOptionDto;
-import com.liberty52.product.service.controller.dto.DeliveryOptionFeeModify;
+import java.time.LocalDateTime;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(DeliveryOptionController.class)
 class DeliveryOptionControllerTest {
 
 	@MockBean
 	private DeliveryOptionRetrieveServiceImpl deliveryOptionRetrieveService;
-	@MockBean
-	private DeliveryOptionModifyServiceImpl deliveryOptionModifyService;
 
 	@Autowired
 	private MockMvc mvc;
-
-	@Autowired
-	private ObjectMapper objectMapper;
 
 	//DeliveryOptionRetrieve
 	@Test
@@ -54,78 +43,4 @@ class DeliveryOptionControllerTest {
 			.andExpect(jsonPath("$.feeUpdatedAt").value(now.toString()));
 	}
 
-	//DeliveryOptionModify
-	@Test
-	@DisplayName("기본 배송비 수정")
-	void updateDefaultDeliveryFee() throws Exception {
-		// given
-		LocalDateTime now = LocalDateTime.now();
-		given(deliveryOptionModifyService.updateDefaultDeliveryFeeByAdmin(anyString(), anyInt()))
-			.willReturn(DeliveryOptionDto.builder()
-				.id(1L)
-				.fee(200_000)
-				.feeUpdatedAt(now)
-				.build());
-		// when
-		// then
-		mvc.perform(patch("/admin/options/delivery/fee")
-				.contentType(MediaType.APPLICATION_JSON)
-				.header(HttpHeaders.AUTHORIZATION, "ADMIN_ID")
-				.header("LB-Role", "ADMIN")
-				.content(objectMapper.writeValueAsString(
-					new DeliveryOptionFeeModify.Request(200_000)
-				)))
-			.andDo(print())
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.fee").value("200,000"))
-			.andExpect(jsonPath("$.feeUpdatedAt").value(now.toString()));
-	}
-
-	@Test
-	@DisplayName("기본 배송비 수정 - 배송비 최소값 불충족")
-	void updatedDefaultDeliveryFee_whenFeeIsMinus() throws Exception {
-		// given
-		// when
-		// then
-		mvc.perform(patch("/admin/options/delivery/fee")
-				.contentType(MediaType.APPLICATION_JSON)
-				.header(HttpHeaders.AUTHORIZATION, "ADMIN_ID")
-				.header("LB-Role", "ADMIN")
-				.content(objectMapper.writeValueAsString(
-					new DeliveryOptionFeeModify.Request(-100_000)
-				)))
-			.andDo(print())
-			.andExpect(status().isBadRequest());
-	}
-
-	@Test
-	@DisplayName("기본 배송비 수정 - 배송비 최댓값 불충족")
-	void updatedDefaultDeliveryFee_whenFeeIsOverHundred() throws Exception {
-		// given
-		// when
-		// then
-		mvc.perform(patch("/admin/options/delivery/fee")
-				.contentType(MediaType.APPLICATION_JSON)
-				.header(HttpHeaders.AUTHORIZATION, "ADMIN_ID")
-				.header("LB-Role", "ADMIN")
-				.content(objectMapper.writeValueAsString(
-					new DeliveryOptionFeeModify.Request(1_000_001)
-				)))
-			.andDo(print())
-			.andExpect(status().isBadRequest());
-	}
-
-	@Test
-	@DisplayName("기본 배송비 수정 - 배송비 Null")
-	void updatedDefaultDeliveryFee_whenFeeIsNull() throws Exception {
-		// given
-		// when
-		// then
-		mvc.perform(patch("/admin/options/delivery/fee")
-				.contentType(MediaType.APPLICATION_JSON)
-				.header(HttpHeaders.AUTHORIZATION, "ADMIN_ID")
-				.header("LB-Role", "ADMIN"))
-			.andDo(print())
-			.andExpect(status().isBadRequest());
-	}
 }
