ceb64491996722c33dbf88fda3397ad25c72ef23
ansm6358 <48744386+ansm6358@users.noreply.github.com>
2023-11-20 13:48:55 +0900

    라이선스 상품 장바구니 조회 (#384)
    * 라이선스 상품 장바구니 조회
    
    * merge dev

--- liberty-main/src/main/java/com/liberty52/main/service/applicationservice/impl/CartItemRetrieveServiceImpl.java
+++ liberty-main/src/main/java/com/liberty52/main/service/applicationservice/impl/CartItemRetrieveServiceImpl.java
@@ -47,7 +47,13 @@ public class CartItemRetrieveServiceImpl implements CartItemRetrieveService {
 
         for (CustomProduct cartItem : cartItemList) {
             Product product = cartItem.getProduct();
-            CartItemResponse cartItemResponse = CartItemResponse.of(cartItem.getId(), product.getName(), cartItem.getUserCustomPictureUrl(), product.getPrice(), cartItem.getQuantity(), getCartOptionList(cartItem.getOptions()), product.getDeliveryOption().getCourierName(), product.getDeliveryOption().getFee());
+            CartItemResponse cartItemResponse;
+            if(product.isCustom()){
+                cartItemResponse  = CartItemResponse.of(cartItem.getId(), product.getName(), cartItem.getUserCustomPictureUrl(), product.getPrice(), cartItem.getQuantity(), getCartOptionList(cartItem.getOptions()), product.getDeliveryOption().getCourierName(),product.getDeliveryOption().getFee(), product.isCustom());
+
+            } else {
+                cartItemResponse  = CartItemResponse.of(cartItem.getId(), product.getName(), cartItem.getCustomLicenseOption().getLicenseOptionDetail().getArtUrl(), product.getPrice(), cartItem.getQuantity(), getCartOptionList(cartItem.getOptions()), product.getDeliveryOption().getCourierName(),product.getDeliveryOption().getFee(), product.isCustom());
+            }
             cartItemResponseList.add(cartItemResponse);
         }
         return cartItemResponseList;
