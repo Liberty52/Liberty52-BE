cd817960cd04ece8493ceee024cc164f57a923a0
DryRains <dryrain_@naver.com>
2023-11-21 21:25:15 +0900

    매출조회 메소드타입 변경

--- liberty-main-service/src/main/java/com/liberty52/main/service/controller/SalesAdminController.java
+++ liberty-main-service/src/main/java/com/liberty52/main/service/controller/SalesAdminController.java
@@ -9,10 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "매출", description = "매출 관련 API를 제공합니다")
 @RestController
@@ -22,7 +19,7 @@ public class SalesAdminController {
     private final SalesRetrieveService salesRetrieveService;
 
     @Operation(summary = "매출 조회", description = "조건에 맞는 매출과 판매량을 조회합니다.")
-    @GetMapping("/admin/sales")
+    @PostMapping("/admin/sales")
     public ResponseEntity<SalesResponseDto> retrieveSalesByAdmin(@RequestHeader("LB-Role") String role, @RequestBody SalesRequestDto salesRequestDto) {
         return ResponseEntity.status(HttpStatus.OK).body(salesRetrieveService.retrieveSales(role, salesRequestDto));
     }
