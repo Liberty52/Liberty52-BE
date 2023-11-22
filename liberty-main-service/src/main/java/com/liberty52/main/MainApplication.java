e26d5ab6fa4111da932cf09570bdd5b336aec5f4
ImKunYoung <ur2kunyoung2@outlook.com>
2023-11-23 05:17:25 +0900

    refactor(#432): Moved 'CRLFLogConverter' from 'com.liberty52.auth.global.utils' to 'com.liberty52.common.utils'.

--- liberty-main-service/src/main/java/com/liberty52/main/MainApplication.java
+++ liberty-main-service/src/main/java/com/liberty52/main/MainApplication.java
@@ -1,6 +1,6 @@
 package com.liberty52.main;
 
-import com.liberty52.main.global.util.CRLFLogConverter;
+import com.liberty52.common.utils.CRLFLogConverter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
