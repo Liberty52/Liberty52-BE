d2c0710d7c7c76ec6b431a7a5b5f1e68a8e40933
ImKunYoung <ur2kunyoung2@outlook.com>
2023-11-23 03:44:55 +0900

    test(#432): Add ArchUnit test for controller naming convention.

---
+++ liberty-auth-service/src/test/java/com/liberty52/auth/ConventionTest.java
@@ -0,0 +1,40 @@
+package com.liberty52.auth;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+class ConventionTest {
+
+    JavaClasses javaClasses;
+
+    @BeforeEach
+    public void beforeEach() {
+        javaClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.liberty52.auth");
+    }
+
+    @Test
+    @DisplayName("web.rest 패키지 내의 클래스는 Controller로 끝나고, RestController 어노테이션을 가지고 있어야 합니다")
+    void controllerClassTest() {
+        ArchRule rule = classes()
+                .that().resideInAnyPackage("..web.rest")
+                .should().haveSimpleNameEndingWith("Controller");
+
+        ArchRule annotationRule = classes()
+                .that().resideInAnyPackage("..web.rest")
+                .should().beAnnotatedWith(RestController.class);
+
+        rule.check(javaClasses);
+        annotationRule.check(javaClasses);
+    }
+
+}
