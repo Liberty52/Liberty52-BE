16237b3b5bb9e7d7c92ae9dfcee9979a7ffd73f3
DryRains <dryrain_@naver.com>
2023-11-27 20:53:14 +0900

    매출조회 응답형식 수정

--- liberty-main-service/src/main/java/com/liberty52/main/service/controller/dto/SalesResponseDto.java
+++ liberty-main-service/src/main/java/com/liberty52/main/service/controller/dto/SalesResponseDto.java
@@ -1,25 +1,30 @@
 package com.liberty52.main.service.controller.dto;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SalesResponseDto {
     private Long totalSalesMoney;
     private Long totalSalesQuantity;
-    private Map<String, Map<String, Object>> monthlySales = new HashMap<>();
+    private List<MonthlySales> monthlySales = new ArrayList<>();
 
     @Builder
-    public SalesResponseDto(Long totalSalesMoney, Long totalSalesQuantity, Map<String, Map<String, Object>> monthlySales) {
+    public SalesResponseDto(Long totalSalesMoney, Long totalSalesQuantity, List<MonthlySales> monthlySales) {
         this.totalSalesMoney = totalSalesMoney;
         this.totalSalesQuantity = totalSalesQuantity;
         this.monthlySales = monthlySales;
     }
 
+    @Setter
+    @Getter
+    public static class MonthlySales {
+        private String year;
+        private String month;
+        private Long salesMoney;
+        private Integer salesQuantity;
+    }
 }
