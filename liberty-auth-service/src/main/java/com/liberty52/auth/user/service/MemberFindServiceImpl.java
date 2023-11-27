4ff13cbd69ec7d3927695141dd1b91f55855dfcc
ImKunYoung <ur2kunyoung2@outlook.com>
2023-11-27 12:39:37 +0900

    refactor(#467): Integrated Feign client for user email retrieval in the 'getEmailList' method.

--- liberty-auth-service/src/main/java/com/liberty52/auth/user/service/MemberFindServiceImpl.java
+++ liberty-auth-service/src/main/java/com/liberty52/auth/user/service/MemberFindServiceImpl.java
@@ -5,26 +5,36 @@ import com.liberty52.auth.user.entity.Auth;
 import com.liberty52.auth.user.repository.AuthRepository;
 import com.liberty52.auth.user.web.dto.FindRequestDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
+@Slf4j
 public class MemberFindServiceImpl implements MemberFindService {
 
-  private final AuthRepository authRepository;
+    private final AuthRepository authRepository;
 
-  @Override
-  public List<String> findEmailByNameAndPhoneNumber(FindRequestDto dto) {
-    List<Auth> authList = authRepository.findEmailByNameAndPhoneNumber(dto.getName(), dto.getPhoneNumber());
-    if (authList.isEmpty()) {
-      throw new InvalidIdOrPhoneNumberException();
+    @Override
+    public List<String> findEmailByNameAndPhoneNumber(FindRequestDto dto) {
+        List<Auth> authList = authRepository.findEmailByNameAndPhoneNumber(dto.getName(), dto.getPhoneNumber());
+        if (authList.isEmpty()) {
+            throw new InvalidIdOrPhoneNumberException();
+        }
+        return authList.stream()
+                .map(Auth::getEmail)
+                .toList();
     }
-    return authList.stream()
-        .map(Auth::getEmail)
-        .toList();
-  }
+
+    @Override
+    public Map<String, String> getUserInfosByWriterIds(List<String> writerIds) {
+        return authRepository.findAllById(writerIds).stream()
+                .collect(Collectors.toMap(Auth::getId, Auth::getEmail));
+    }
 }
