06c4b952bf8012b70eeceeaa7859d906a3c16000
ImKunYoung <ur2kunyoung2@outlook.com>
2023-11-30 08:51:08 +0900

    ref(#485): Remove unnecessary code

--- liberty-main-service/src/test/java/com/liberty52/main/service/applicationservice/ProductInfoRetrieveServiceImplTest.java
+++
@@ -1,5 +0,0 @@
-package com.liberty52.main.service.applicationservice;
-
-class ProductInfoRetrieveServiceImplTest {
-
-}
--- liberty-main-service/src/test/java/com/liberty52/main/service/applicationservice/impl/ImageGenerationServiceImplTest.java
+++
@@ -1,8 +0,0 @@
-package com.liberty52.main.service.applicationservice.impl;
-
-class ImageGenerationServiceImplTest {
-
-    void generate() {
-        // post man 테스트로 대체.
-    }
-}
